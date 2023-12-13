df8cca983cab0bf62e27b3b12dfe04a4139dbd1c
erenaygunn <eaygun178@gmail.com>
2023-12-13 14:39:50 +0300

    background color

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
+    import Header from './components/Header.vue';
 </script>
 
 <template>
+    <Header></Header>
 </template>
 
-<style scoped>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
 
+    body {
+        background-color: #F1F6F9;
+    }
 </style>
