f6199ada26b4cada64cb4e7d8eeb9272c7c77c85
erenaygunn <eaygun178@gmail.com>
2023-12-14 19:23:58 +0300

    button animation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,19 @@
 <template>
     <header>
         <h1>Quotter</h1>
-        <h5>This a public quote platform where you can post and read quotes.</h5>
-        <button>Write a Quote</button>
+        <h5>This is a public quote platform where you can post and read quotes.</h5>
+        <button>Write a Quote<Icon icon="jam:write-f" class="icon"/></button>
     </header>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue';
+
 export default {
-    
-}
+	components: {
+		Icon,
+	},
+};
 </script>
 
 <style scoped>
@@ -28,7 +32,7 @@ export default {
         font-size: 5rem;
         font-weight: 700;
         font-family: 'Comfortaa', sans-serif;
-        color: #394867;
+        color: #212A3E;
     }
 
     header h5 {
@@ -46,5 +50,29 @@ export default {
         border-radius: 50px;
         border: none;
         background-color: #212A3E;
+        display: flex;
+        align-items: center;
+        transition: all 0.3s;
+        position: relative;
+        width: 215px;
+    }
+    header button:hover {
+        cursor: pointer;
+        opacity: 90%;
+        width: 250px
+    }
+
+    .icon {
+        opacity: 0;
+        position: absolute;
+        padding-left: 10px;
+        margin-top: 2px;
+        transition: all 0.3s;
+        left: 0;
+    }
+
+    header button:hover .icon {
+        opacity: 100%;
+        left: 190px;
     }
 </style>
