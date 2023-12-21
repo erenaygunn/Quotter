6322f3dc6fa7b6d3322d4eb690112c8c22a1af1e
erenaygunn <eaygun178@gmail.com>
2023-12-21 13:25:03 +0300

    import fix

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
     import Header from './components/Header.vue';
+    import Quotes from './components/Quotes.vue';
 </script>
 
 <template>
     <Header></Header>
+    <Quotes></Quotes>
 </template>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;700&display=swap');
+
     *{
         margin: 0;
         padding: 0;
