f3389e4a184d5318f703aaecbfe29afb65899a9d
erenaygunn <eaygun178@gmail.com>
2023-12-21 13:24:54 +0300

    quote styling

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -25,27 +25,36 @@
     display: flex;
     flex-direction: column;
     padding: 20px;
-    background-color: #212A3E;
+    background-color: #394867;
     border-radius: 30px;
     margin: 30px;
   }
   
   .quote-text {
-    font-size: 18px;
+    font-size: 1.2rem;
     margin-bottom: 5px;
     color: #F1F6F9;
+    font-family: 'Comfortaa', sans-serif;
   }
   
   .quote-author {
     font-style: italic;
     color: #9BA4B5;
     margin-bottom: 10px;
+    font-family: 'Ubuntu', sans-serif;
   }
   
   .quote-metadata {
     display: flex;
     justify-content: space-between;
     color: #9BA4B5;
+    font-family: 'Ubuntu', sans-serif;
+  }
+
+  @media screen and (max-width: 380px) {
+    .quote-text, .quote-author, .quote-metadata {
+        font-size: 0.8rem;
+    }
   }
   
   </style>
