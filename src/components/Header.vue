802fa441b002deb4087b0b76f4352f3f80acd5c9
erenaygunn <eaygun178@gmail.com>
2023-12-23 20:26:20 +0300

    margin to padding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,7 +22,7 @@ export default {
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        margin-top: 50px;
+        padding-top: 50px;
         text-align: center;
     }
 
