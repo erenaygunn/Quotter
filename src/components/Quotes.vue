e23393f072fb62b0e161dc5d4b1495b316ccd7a5
erenaygunn <eaygun178@gmail.com>
2023-12-23 13:01:53 +0300

    quotes container

---
+++ src/components/Quotes.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="quotes">
+      <Quote v-for="quote in quotes" :key="quote.id" :quote="quote" />
+    </div>
+</template>
+  
+  <script>
+  import Quote from './Quote.vue';
+  
+  export default {
+    components: {
+      Quote
+    },
+    data() {
+      return {
+        quotes: [
+          {
+            id: 1,
+            text: "The only limit to our realization of tomorrow will be our doubts of today.",
+            author: "Franklin D. Roosevelt",
+            likes: 42,
+            uploader: "Eren",
+            date: "21.12.2023"
+          },
+          {
+            id: 1,
+            text: "The only limit to our realization of tomorrow will be our doubts of today.",
+            author: "Franklin D. Roosevelt",
+            likes: 42,
+            uploader: "Eren",
+            date: "21.12.2023"
+          },
+          {
+            id: 1,
+            text: "The only limit to our realization of tomorrow will be our doubts of today.The only limit to our realization of tomorrow will be our doubts of today.The only limit to our realization of tomorrow will be our doubts of today.",
+            author: "Franklin D. Roosevelt",
+            likes: 42,
+            uploader: "Eren",
+            date: "21.12.2023"
+          },
+          {
+            id: 1,
+            text: "The only limit to our realization of tomorrow will be our doubts of today.",
+            author: "Franklin D. Roosevelt",
+            likes: 42,
+            uploader: "Eren",
+            date: "21.12.2023"
+          },
+          // Add more quotes as needed
+        ]
+      };
+    }
+  };
+  </script>
+
+<style scoped>
+.quotes {
+  display: flex;
+  padding: 50px 20px;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+@media screen and (max-width: 425px) {
+  .quotes {
+    padding: 30px 20px;
+  }
+}
+
+</style>
