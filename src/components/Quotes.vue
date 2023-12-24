a813c17764520fe4e1828736ab18ea67fe2aba73
erenaygunn <eaygun178@gmail.com>
2023-12-24 14:56:21 +0300

    search bar logic

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="quotes">
-      <Quote v-for="quote in quotes" :key="quote.id" :quote="quote" />
+    <div class="container">
+      <div class="search-and-sort">
+        <input v-model="searchQuery" type="text" placeholder="Search..." />
+        <select v-model="sortOption">
+          <option value="date">Sort by Date</option>
+          <option value="likes">Sort by Likes</option>
+          <option value="length">Sort by Length</option>
+        </select>
+      </div>
+      <div class="quotes">
+        <Quote v-for="quote in quotes" :key="quote.id" :quote="quote" />
+      </div>
     </div>
 </template>
   
@@ -11,6 +21,7 @@
     components: {
       Quote
     },
+    
     data() {
       return {
         quotes: [
@@ -47,27 +58,73 @@
             date: "21.12.2023"
           },
           // Add more quotes as needed
-        ]
+        ],
+        searchQuery: '',
+        sortOption: 'date'
       };
+    },
+
+    computed: {
+    filteredQuotes() {
+      let filtered = [...this.quotes];
+
+      // Search logic
+      if (this.searchQuery.trim() !== '') {
+        const query = this.searchQuery.toLowerCase();
+        filtered = filtered.filter(
+          (quote) =>
+            quote.text.toLowerCase().includes(query) ||
+            quote.author.toLowerCase().includes(query) ||
+            quote.uploader.toLowerCase().includes(query)
+        );
+      }
+      // Sorting logic
+      switch (this.sortOption) {
+        case 'date':
+          filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
+          break;
+        case 'likes':
+          filtered.sort((a, b) => b.likes - a.likes);
+          break;
+        case 'length':
+          filtered.sort((a, b) => a.text.length - b.text.length);
+          break;
+        default:
+          break;
+      }
+
+      return filtered;
+
     }
+  }
   };
   </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+  align-items: center;
+}
+
 .quotes {
   display: flex;
-  padding: 50px 20px 45px 20px;
-  margin: 50px 40px;
+  margin: 30px 40px;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   border-radius: 40px;
 }
 
+
 @media screen and (max-width: 425px) {
   .quotes {
-    padding: 25px 15px;
     margin: 25px 20px;
+  }
+
+  .container {
+    margin-top: 30px;
   }
 }
 
