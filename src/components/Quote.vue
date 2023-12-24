61bbaeee2e2d2e1fbf4ab478c865f7b9d0b27152
erenaygunn <eaygun178@gmail.com>
2023-12-24 14:19:47 +0300

    align items center

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -58,6 +58,7 @@
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    align-items: center;
     color: #9BA4B5;
     font-family: 'Ubuntu', sans-serif;
     white-space: nowrap;
