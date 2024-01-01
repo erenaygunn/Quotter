f21b8e8763657717e5ca5c7abb478179df32c12f
erenaygunn <eaygun178@gmail.com>
2024-01-01 12:49:22 +0300

    date sort fix

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -93,7 +93,7 @@
       // Sorting logic
       switch (this.sortOption) {
         case 'date':
-          filtered.sort((a, b) => b.date - a.date);
+          filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
           break;
         case 'likes':
           filtered.sort((a, b) => b.likes - a.likes);
